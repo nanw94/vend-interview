4f1c64588dea23ff18643a35271425b96d8c073e
NathanW <wangnan8194@gmail.com>
2018-08-24 09:24:15 +1200

    correction - bad role id

--- cypress/testData/users.js
+++ cypress/testData/users.js
@@ -6,6 +6,7 @@ export const AdminPassword = 'wangnan2018';
 export const DefaultToken = 'Bearer KiQSsELLtocySqhV1FKry_x6PdmeL8jZnEgiFjmr';
 export const ManagerRoleID ='1032303977849458688';
 export const BadToken = 'Bearer ziQSsELLtocySqhV1FKry_x6PdmeL8jZnEgiFjmr';
+export const BadRoleID = '9032303977849458688';
 
 //Manager
 export const ManagerUserName = 'Manager';
